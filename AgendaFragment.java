b0b46696737e107f14f7ad1414594819109acf63
Tiago Bolota <tiagobolota05@gmail.com>
2025-10-29 01:37:32 +0000

    Add files via upload

---
+++ AgendaFragment.java
@@ -0,0 +1,15 @@
+package com.example.listadetarefas;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AgendaFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_agenda, container, false);
+    }
+}
